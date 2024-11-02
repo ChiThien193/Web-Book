66a0e1c90daa788cfd7111735757d985cd49a513
ChiThien193 <chithientb2022@gmail.com>
2024-11-03 00:59:36 +0700

    add initial project setup with configuration files, utility functions, and basic routes

---
+++ client/src/app/(group login register)/register/page.tsx
@@ -0,0 +1,14 @@
+import RegisterFrom from '@/app/(group login register)/register/register-form'
+import React from 'react'
+
+
+const Register = () => {
+return (
+  <div>
+    <h1>Đăng kí</h1>
+    <RegisterFrom />
+  </div>
+)
+}
+
+export default Register
